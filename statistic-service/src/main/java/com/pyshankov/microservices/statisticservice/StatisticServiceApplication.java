ac457a5baaa2a4f9982332b4646121ab9a25b848
bartromg <fraps.zeroo@gmail.com>
2018-06-26 00:33:59 +0300

    Add dependencies for static-server

--- statistic-service/src/main/java/com/pyshankov/microservices/statisticservice/StatisticServiceApplication.java
+++ statistic-service/src/main/java/com/pyshankov/microservices/statisticservice/StatisticServiceApplication.java
@@ -1,11 +1,8 @@
 package com.pyshankov.microservices.statisticservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,16 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatisticServiceApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(StatisticServiceApplication.class, args);
     }
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping(value = "/test")
-    public String getTest() {
-        return "test from Statistic";
-    }
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+//
+//    @RequestMapping(value = "/test")
+//    public String getTest() {
+//        return "test from Statistic";
+//    }
 
 }
