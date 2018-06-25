68ab68553aef9b83d9e38936c8347137d91a7fd3
bartromg <fraps.zeroo@gmail.com>
2018-06-26 00:32:13 +0300

    Create controller for static-server module

--- statistic-service/src/main/java/com/pyshankov/microservices/statisticservice/controller/StatisticController.java
+++ statistic-service/src/main/java/com/pyshankov/microservices/statisticservice/controller/StatisticController.java
@@ -1,7 +1,43 @@
 package com.pyshankov.microservices.statisticservice.controller;
 
+import com.pyshankov.microservices.domain.Event;
+import com.pyshankov.microservices.statisticservice.service.StatisticService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * Created by valeriyartemenko on 06.05.18.
  */
+@RestController
 public class StatisticController {
+    @Autowired
+    StatisticService statisticService;
+
+    @RequestMapping("/allevents")
+    public List<Event> eventByName(@RequestParam(value="name") String name) {
+        List<Event> events = statisticService.getEventsByName(name);
+        if(events.isEmpty()){
+            return null;
+        }
+        return events;
+    }
+
+    @RequestMapping("/newevent")
+    public void newEvent(Event event) {
+        statisticService.handleEvent(event);
+    }
+
+
+    @RequestMapping("/delete")
+    public void deletAllEvents() {
+        statisticService.clearData();
+    }
+
+
+
 }
+
